7f2cc6ebcb35eb3fc900c9f68e6851a168eef8a3
Deirdre Allison <deirdre9@gmail.com>
2014-02-12 12:32:16 -0800

    created gift guide app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GiftGuide::Application.config.secret_key_base = '42e08230e0b5f7270ccd2494b5a87e7de4ecbdc401b40119aea787283616639a7ab8127472842a6013881434f5f61b59d1ecf104b4040471228793a686482d93'
